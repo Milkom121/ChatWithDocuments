6f7ec11373cda4c7245a31686978a55a70530912
Mario <mattia.digitalmosaik@gmail.com>
2024-06-17 23:31:34 +0200

    refactor: Update data_loader.py to adjust chunk overlap in load_and_split_text function

--- chatwithdocuments/data_loader.py
+++ chatwithdocuments/data_loader.py
@@ -2,7 +2,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_core.documents.base import Document
 
 # Funzione per caricare e dividere il testo
-def load_and_split_text(content, separator=".", chunk_size=1000, chunk_overlap=0):
+def load_and_split_text(content, separator=".", chunk_size=1000, chunk_overlap=200):
     text_splitter = CharacterTextSplitter(
         separator=separator,
         chunk_size=chunk_size,
